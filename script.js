5a164edfa36dae2af41a1c3362a70861e2bc7e18
navin <95286412+sahnavin123@users.noreply.github.com>
2023-04-28 14:17:25 +0530

    wrote javascript code for the game

---
+++ script.js
@@ -0,0 +1,69 @@
+let choices = ["rock", "paper", "scissors"];
+let getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
+let userPoint = 0;
+let computerPoint = 0;
+
+const playRound = (playerSelection, computerSelection) => {
+    const userChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+    switch (userChoice) {
+        case 'rock':
+            switch (computerChoice) {
+                case 'paper':
+                    return ("Computer");
+                case 'scissors':
+                    return ("you");
+                default:
+                    return ("draw");
+            }
+        case 'paper':
+            switch (computerChoice) {
+                case 'scissors':
+                    return ("Computer");
+                case 'rock':
+                    return ("you");
+                default:
+                    return ("draw");
+            }
+        case 'scissors':
+            switch (computerChoice) {
+                case 'rock':
+                    return ("Computer");
+                case 'paper':
+                    return ("you");
+                default:
+                    return ("draw");
+            }
+        default:
+            return ("The choice is invalid. Please write valid input");
+    }
+}
+
+const printResult = (result) => {
+    switch (result) {
+        case 'you':
+            userPoint++;
+            console.log(`You have won with ${userPoint} points`);
+            break;
+        case 'Computer':
+            computerPoint++;
+            console.log(`Computer has won with ${computerPoint} points`);
+            break;
+        case 'draw':
+            console.log("The game is draw");
+            break;
+        default:
+            console.log("invalid choice, please make a good choice");
+    }
+}
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("enter your choice");
+        const computerSelection = getComputerChoice();
+        const result = playRound(playerSelection, computerSelection);
+        printResult(result);
+    }
+}
+
+game();
